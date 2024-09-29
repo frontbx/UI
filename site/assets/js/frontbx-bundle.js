1f82f1f310d1de2bcdac0ebbdb2aa3ac44cb02c2
Joe Howard <joe.howard.au@icloud.com>
2024-09-30 09:50:46 +1000

    Updated in_dom

--- site/assets/js/frontbx-bundle.js
+++ site/assets/js/frontbx-bundle.js
@@ -6340,16 +6340,23 @@
      */
     _.prototype.in_dom = function(element)
     {
+        // Element types
         if (element === window || element === document || element === document.body || element === document.documentElement)
         {
             return true;
         }
     
+        // Not an HTML element
         if (!this.is_htmlElement(element))
         {
             return false;
         }
     
+        // No parent node
+        if (!element.parentNode) return false;
+    
+        // Matching by selector is faster than traversing up the
+        // DOM tree - especially if element is deeply nested.
         let selector = element.tagName.toLowerCase();
     
         if (element.id && element.id.trim() !== '')
@@ -6362,8 +6369,19 @@
     
             selector = classNames.length === 1 ? `.${classNames[0]}` : `.${classNames.join('.')}`;
         }
+        else
+        {
+            let classNames = this.class_names(element.parentNode);
+    
+            if (classNames.length >= 1) selector = classNames.length === 1 ? `.${classNames[0]} ${selector}` : `.${classNames.join('.')} ${selector}`;
+        }
     
         let matches = this.find_all(selector);
+    
+        // Fast check
+        if (matches.length === 1 && matches[0] === element) return true;
+    
+        // Loop through
         let ret = false;
     
         this.each(matches, (i, match) =>
